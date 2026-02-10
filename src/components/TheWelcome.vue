5b4fe1c05f8860369f85f2de74d98a3b930832fc
ylcruzdev <ylcruz.dev@gmail.com>
2026-02-10 12:53:47 +0100

    update home

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,8 +1,21 @@
 <template>
-  <h1>
-    Hola, aqui encontraras ejemplos sobre algunos conceptos basicos de vue.
-  </h1>
-  <h2>
-    Navega por los ejemplos para ver su funcionamiento y luego revisa el codigo para ver como funciona
-  </h2>
+  <div class="d-flex xy-center">
+    <div class="xy-center">
+      <span class="material-icons md-48">arrow_back</span>
+    </div>
+    <div>
+      <h1>
+        Hola, aqui encontraras ejemplos sobre algunos conceptos basicos de vue.
+      </h1>
+      <h2>
+        Navega por los ejemplos para ver su funcionamiento y luego revisa el codigo para ver como funciona
+      </h2>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.md-48 {
+  font-size: 4rem;
+}
+</style>
